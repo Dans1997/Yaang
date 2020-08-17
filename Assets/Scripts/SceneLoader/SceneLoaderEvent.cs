10b6a125011cb6bdd69af037fb7a233decefcb17
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-08-17 20:20:02 -0300

    Splash Screen Added

---
+++ Assets/Scripts/SceneLoader/SceneLoaderEvent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoaderEvent : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+
+    public void LoadNextScene(float duration) => StartCoroutine(CallLoader(duration));
+
+    private IEnumerator CallLoader(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        sceneLoader.LoadNextScene();
+    }
+}
