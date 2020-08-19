736becd9d2c16513a0712b16cdfbc4bb4177c94e
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-08-19 20:51:16 -0300

    Simple Save System Code
     - Not working, but it's already set up in case we need it.
    
    PS.: Consider using the free asset Easy Save for saving

---
+++ Assets/Scripts/SaveSystem/PlayerData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    //TODO: Everything we need to save here.
+
+    public PlayerData(PlayerController player)
+    {
+        // Initialize everything here
+    }
+}
---
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem 
+{
+    const string pathEnd = "/player.yaang";
+
+    public static void SavePlayer (PlayerController player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + pathEnd;
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData playerData = new PlayerData(player);
+
+        formatter.Serialize(stream, playerData);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayer()
+    {
+        string path = Application.persistentDataPath + pathEnd;
+        if(File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            PlayerData playerData = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+
+            return playerData;
+        }
+        else
+        {
+            Debug.LogError("Save File not found in " + path);
+            return null;
+        }
+    }
+}
