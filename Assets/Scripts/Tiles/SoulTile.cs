792c89be2baf5005a7319ab94a834ba105412540
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-09-01 11:36:59 -0300

    Levels 81-90
     - Soul Tiles can now teleport you to any type of tile
     - Soul Cave Visuals Added

--- Assets/Scripts/Tiles/SoulTile.cs
+++ Assets/Scripts/Tiles/SoulTile.cs
@@ -7,21 +7,29 @@ public class SoulTile : TilePath
 {
     // Soul Tile -> This one doesn't hurt you, but it will get you lost.
 
+    // State
+    [SerializeField] bool canBounce = false; // If enabled, the player will bounce between Soul Tiles
+
     // Cached Components
-    [SerializeField] SoulTile pairTile;
+    [SerializeField] TilePath pairTile = null;
+    SoulTile soulTile = null;
     Animator soulTileAnimator;
     AudioManager audioManager;
     Camera mainCamera;
+    PathManager pathManager;
 
     private void Start()
     {
-        if (pairTile && !pairTile.pairTile)
-        {
-            pairTile.pairTile = this;
-        }
+        if (pairTile == null) Debug.LogWarning("Soul Tile has no pair to teleport to!");
         soulTileAnimator = GetComponent<Animator>();
         audioManager = AudioManager.AudioManagerInstance;
         mainCamera = Camera.main;
+        pathManager = PathManager.PathManagerInstance;
+
+        if (pairTile.GetType() == typeof(SoulTile))
+        {
+            soulTile = pairTile.GetComponent<SoulTile>();
+        }
     }
 
     public override string GetName() => "Soul Tile";
@@ -49,7 +57,11 @@ public class SoulTile : TilePath
         yield return new WaitForSeconds(1f);
 
         audioManager.PlaySound(AudioManager.SoundKey.TeleportOut);
-        pairTile.soulTileAnimator.Play("Soul_Teleport_Out", 0);
+        
+        if(soulTile)
+        {
+            soulTile.soulTileAnimator.Play("Soul_Teleport_Out", 0);
+        }
 
         player.enabled = true;
         player.transform.position = pairTile.transform.position;
@@ -57,9 +69,25 @@ public class SoulTile : TilePath
 
         if (!pairTile.WasVisited())
         {
-            pairTile.TurnOn();
-            pairTile.SetVisit(true);
-            PathManager.PathManagerInstance.IncrementVisitedCount();
+            if (soulTile)
+            {
+                if (soulTile.canBounce)
+                {
+                    player.enabled = false;
+                    yield return new WaitForSeconds(1f);
+                    pathManager.CheckPlayerPosition();
+                } 
+                else
+                {
+                    pairTile.TurnOn();
+                    pairTile.SetVisit(true);
+                    pathManager.IncrementVisitedCount();
+                }
+            }
+            else
+            {
+                pathManager.CheckPlayerPosition();
+            }
         }
     }
 
@@ -68,5 +96,23 @@ public class SoulTile : TilePath
         //TODO: Different Turn On Sound for the Soul Tile
         int randomSound = Random.Range((int)AudioManager.SoundKey.TileLightUp1, (int)AudioManager.SoundKey.TileLightUp4);
         AudioManager.AudioManagerInstance.PlaySound((AudioManager.SoundKey)randomSound);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!pairTile) return;
+        Gizmos.color = Color.green;
+
+        Vector3 pos = transform.position;
+        Vector3 direction = (pairTile.transform.position - pos)/2;
+        float arrowHeadAngle = 30f;
+        float arrowHeadLength = 0.25f;
+
+        Gizmos.DrawRay(pos, direction);
+
+        Vector3 right = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 + arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Vector3 left = Quaternion.LookRotation(direction) * Quaternion.Euler(0, 180 - arrowHeadAngle, 0) * new Vector3(0, 0, 1);
+        Gizmos.DrawRay(pos + direction, right * arrowHeadLength);
+        Gizmos.DrawRay(pos + direction, left * arrowHeadLength);
     }
 }
