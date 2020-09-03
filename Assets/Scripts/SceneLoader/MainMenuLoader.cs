7641760c9e9f09435d208eae40cd7893ababa055
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-09-03 14:44:18 -0300

    Quick Fix to The Runtime Menu

---
+++ Assets/Scripts/SceneLoader/MainMenuLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuLoader : MonoBehaviour
+{
+    SceneLoader sceneLoader;
+
+    void Start() => sceneLoader = FindObjectOfType<SceneLoader>();
+
+    public void LoadMainMenu() => sceneLoader.LoadMainMenu();
+}
